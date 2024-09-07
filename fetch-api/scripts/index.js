553e0e0951513413943b5de6945ea796d55cf0bc
Jose <jmagarciadiez@gmail.com>
2024-09-07 14:46:49 -0300

    Create reusable fetch function to get products

--- fetch-api/scripts/index.js
+++ fetch-api/scripts/index.js
@@ -1,2 +1,12 @@
-
-
+async function getProducts(url) {
+  try {
+    const res = await fetch(url);
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
