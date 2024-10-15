a21f0b69615556e2d8b6b79c28d404a0a8112171
Jose <jmagarciadiez@gmail.com>
2024-10-15 19:26:28 -0300

    Add simple test code

--- projects/poo-videojuego-rpg/src/index.ts
+++ projects/poo-videojuego-rpg/src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, RPG project!");
+import { Wizard } from "./models/characters";
+import { Box } from "./models/items";
+
+const wizard = new Wizard("Dumbledore");
+
+const box_1 = new Box();
+const box_2 = new Box();
+
+wizard.openBox(box_1);
+wizard.openBox(box_2);
+
+wizard.getSkills();
+
+wizard.attack(wizard.useSkill(2) || undefined);
+
+wizard.getSkills();
